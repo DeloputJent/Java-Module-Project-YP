c33b935fa066b961cc4fe861756755c13cf4d1fe
DeloputJent <enu.w@yandex.ru>
2025-04-01 20:57:55 +0400

    Выполненое задание Евгений Устюжанин Студент курса Android-разработчик. Когорта № 31

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,48 @@
+import java.util.Scanner;
+class Car {
+    public String model="";
+    public int speed;
+    Car (String model, int speed) {
+        this.model=model;
+        this.speed=speed;
+        }
+}
+class Rally {
+    public String Leader="";
+    public int distance=0;
+    public void competition (String competitor, int speed) {
+        if ((speed*24)>distance) {
+            Leader=competitor;
+            distance=speed*24;
+        }
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car[] cars=new Car[3];
+        Rally staart =new Rally();
+        Scanner scanner=new Scanner(System.in);
+        for (int i=0;i<3;i++) {
+            System.out.println("— Введите название машины №"+(i+1)+":");
+            String mod=scanner.next();
+            int carSpeed;
+            while (true) {
+                System.out.println("— Введите скорость машины №" + (i + 1) + ":");
+                if (scanner.hasNextInt()) {
+                    carSpeed = scanner.nextInt();
+                    if ((carSpeed > 0) && (carSpeed <= 250)) break;
+                    else System.out.println("— Неправильная скорость");
+                } else {
+                    System.out.println("— Неправильная скорость");
+                    scanner.nextLine();
+                    scanner.nextLine();
+                }
+            }
+            cars[i]=new Car(mod, carSpeed);
+            staart.competition(cars[i].model, cars[i].speed);
+        }
+        System.out.println("Самая быстрая машина: "+staart.Leader);
+        scanner.close();
     }
 }
